35fff17cd1c80454cfff2182238841a9e89220e3
martin-alexis <alexismartin090@gmail.com>
2024-11-02 16:54:12 -0300

    Se ha puesto un filtro de busqueda en la parte de productos panaderia

--- backend/api/app/routes/productos_route.py
+++ backend/api/app/routes/productos_route.py
@@ -10,11 +10,18 @@ def get_productos():
     controller = ProductosController()
     return controller.get_productos()
 
-@productos_bp.route('/api/productos_panaderia', methods=['GET'])
+@productos_bp.route('/api/buscar', methods=['GET'])
 def get_productos_panaderia():
 
+    busqueda = request.args.get('termino', '')
+
+    precio_min = request.args.get('precio_min', type=float)
+    precio_max = request.args.get('precio_max', type=float)
+    disponible = request.args.get('disponible', type=lambda v: v.lower() == 'true')
+    agotado = request.args.get('agotado', type=lambda v: v.lower() == 'true')
+
     controller = ProductosController()
-    return controller.get_productos_panaderia()
+    return controller.get_productos_panaderia(busqueda, precio_min, precio_max, agotado, disponible)
 
 @productos_bp.route('/api/productos_despensa', methods=['GET'])
 def get_productos_despensa():
